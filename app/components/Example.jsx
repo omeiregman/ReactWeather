dccff77a0ff23c06ef3322b37f2ef01bb963333e
= <=>
2017-09-15 20:36:30 +0100

    Examples style

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
 
-
+var {Link} = require('react-router');
 
 var Example = (props) => {
   return (
 <div>
-    <h3> Examples</h3>
-    <p>Welcome to the example page!</p>
-</div>    
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few example locations to try out</p>
+    <ol>
+      <li>
+        <Link to='/?location=Lagos'>Lagos, Nigeria</Link>
+      </li>
+      <li>
+        <Link to ='/?location=Berlin'>Berlin, Germany</Link>
+      </li>
+    </ol>
+</div>
   );
 }
 module.exports = Example;
